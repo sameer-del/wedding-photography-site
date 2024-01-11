351c9f932d36255d3dbfb319895442cd1380fec4
Sameer Ahamed <122287404+sameer-del@users.noreply.github.com>
2024-01-11 22:24:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+ScrollReveal({
+   reset: true,
+   duration: 2000,
+
+});
+
+ScrollReveal().reveal('#home',{ delay: 100 });
+ScrollReveal().reveal('#gadget',{ delay: 100 });
+ScrollReveal().reveal('#users-review',{ delay: 100 });
+ScrollReveal().reveal('#contact',{ delay: 100 });
